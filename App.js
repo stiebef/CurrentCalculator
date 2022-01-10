0db0a243c53a3ed20b7cf2ee37d192c9337e07bd
Felix Stieberitz <felix.stieberitz@web.de>
2022-01-10 12:50:52 +0200

    style

--- App.js
+++ App.js
@@ -29,9 +29,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:50,
+    marginLeft: 10,
   },
+  field:{
+    marginBottom: 10,
+  }
 });
