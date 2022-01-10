fe9a88d107f154bdc62f5b3a5df06bd2d12ced4e
Felix Stieberitz <felix.stieberitz@web.de>
2022-01-10 13:36:14 +0200

    low

--- App.js
+++ App.js
@@ -3,29 +3,45 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
-  const [euros,setEuros] = useState('');
-  const[pounds, setPounds] = useState(0);
+  const [age,setAge] = useState('');
+  const[lower, setLower] = useState(0);
+  const[lhigher, setHigher] = useState(0);
 
-  function change(text) {
-    setEuros(text)
-    const result = text.replace(',', '.') * 0.9;
-    setPounds(result);
+  function defineLow() {
+    
+    const result = (220 - age) * 0.65;
+    
+    setLower(result);
   }
+
+  function defineHigh() {
+    
+    const result = (220 - age) * 0.85;
+    
+    setHigher(result);
+  }
+//  function change(text) {
+ //   setEuros(text)
+//    const result = text.replace(',', '.') * 0.9;
+//    setPounds(result);
+//  }
 //  function calculate() {
 //    const result = euros.replace(',','.') * 0.9;
 //    setPounds(result);
 //  }
  return( 
-   <View style={styles.container}>
-     <Text style ={styles.field}>Euros</Text>
-    {/*  <TextInput style={styles.field} value={euros} onChangeText={text => setEuros(text)} */}
-    <TextInput style={styles.filed} value={euros} onChangeText={text => change(text)} keyboard-type ='decimal-pad'></TextInput>
+   <View style={styles.container}>   
+    <Text style ={styles.field}>Age</Text>
+      <TextInput style={styles.field} value={age} onChangeText={text => setAge(text)} keyboard-type ='decimal-pad'></TextInput>
+      {/* <TextInput style={styles.filed} value={age} onChangeText={text => change(text)} keyboard-type ='decimal-pad'></TextInput> */}
     
-     <Text style={styles.field}>Punds</Text>
-     <Text style={styles.field}>{pounds.toFixed(2)}</Text>
-    {/* <Button onPress={calculate} title="Calculate"></Button> */}
+      <Text style={styles.field}>HR limit</Text>
+      <TextInput style={styles.field} value={lower} onChangeText={age => setLower()} keyboard-type ='decimal-pad'></TextInput>
+
+
    </View>
- );
+);
+   
    
 
 
